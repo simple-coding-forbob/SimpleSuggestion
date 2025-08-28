9d839a700a07c358b3952bacc4ac2de1b45e7cd0
simple-coding-forbob <96178104+simple-coding-forbob@users.noreply.github.com>
2025-08-28 17:21:09 +0900

    1

--- src/main/java/com/simplecoding/simlesuggestion/es/filedbsuggested/repository/FileDbSuggestedRepository.java
+++ src/main/java/com/simplecoding/simlesuggestion/es/filedbsuggested/repository/FileDbSuggestedRepository.java
@@ -1,7 +1,10 @@
 package com.simplecoding.simlesuggestion.es.filedbsuggested.repository;
 
 import com.simplecoding.simlesuggestion.es.filedbsuggested.entity.FileDbSuggested;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
